5abf35cdbde0fb59346b153c10df604872462963
Billy Cole <cole.billys@gmail.com>
2020-04-04 12:10:16 -0700

    case insensitive option filtering

--- src/components/SearchForm/SearchField/helpers.js
+++ src/components/SearchForm/SearchField/helpers.js
@@ -1,8 +1,10 @@
 import { mockOptions } from '../../../mockData';
 
 /**
- * 
+ * findMatches
+ * @description takes a string and returns a filtered mockOptions array containing only the
+ *              options that contain the string
  */
 export const findMatches = (string) => {
-  return mockOptions.filter(option => option.includes(string));
+  return mockOptions.filter(option => option.toLowerCase().includes(string.toLowerCase()));
 };
